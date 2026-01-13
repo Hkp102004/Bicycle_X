a0e254725e52930b64e3506475c5f2be43507da5
HKP102004 <harsh15062004@gmail.com>
2026-01-13 19:30:59 +0530

    feat: make wheel_position_tracker script and make a trasnform variable to track wheel position

---
+++ Assets/Scripts/wheel_position_tracker.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class wheel_position_tracker : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform wheelPosition; //tracks the position of the wheel
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
