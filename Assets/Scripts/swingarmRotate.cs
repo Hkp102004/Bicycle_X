2b284523dbe4f94a6d8c5c71472ba4b4ac93b3a5
HKP102004 <harsh15062004@gmail.com>
2026-01-21 00:03:21 +0530

    feat: make the rotation logic in swingarmRotate script

--- Assets/Scripts/swingarmRotate.cs
+++ Assets/Scripts/swingarmRotate.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class swingarmRotate : MonoBehaviour
@@ -22,5 +23,12 @@ public class swingarmRotate : MonoBehaviour
             Debug.LogError("Either pivot or wheelfollow is not assigned in swingarmRotate script");
             return;
         }
+
+        transform.position = pivot.position;
+        Vector3 direction = wheelfollow.position - pivot.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0,0,angle);
     }
 }
